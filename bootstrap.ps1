285e6e6c5947d77d1e74f3e565bd77088ace8325
yuri-rage <yuri.rage@gmail.com>
2021-10-07 12:48:38 -0500

    initial commit

---
+++ bootstrap.ps1
@@ -0,0 +1,65 @@
+<#
+ # * bootstrap.ps1
+ #
+ # * Creates a new Python project for VS Code with Yuri's settings
+ #
+ # ! Disclaimer: I'm not a PowerShell power user... 
+ #
+ # * -- Yuri - Oct 2021
+#>
+
+$project_name = "$args"
+
+if ("" -eq $project_name) {
+        Write-Output "Usage: $($MyInvocation.MyCommand.Name) <project name>"
+        Exit 1
+}
+
+$path = Join-Path (Resolve-Path ..) "$project_name"
+
+
+if(Test-Path "$path") {
+        Write-Output "`n$path already exists."
+} else {
+        Write-Output "`nCreating new project: $path"
+        [System.IO.Directory]::CreateDirectory("$path") | Out-Null
+}
+
+if(Test-Path "$path\.vscode") {
+        Write-Output ".vscode exists, not overwriting."
+} else {
+        Write-Output "Copying .vscode folder..."
+        Copy-Item ".\.vscode" -Recurse -Destination "$path"
+}
+
+if(Test-Path "$path\main.py") {
+        Write-Output "main.py exists, not overwriting."
+} else {
+        Write-Output "Copying main.py..."
+        Copy-Item ".\main.py" -Destination "$path"
+}
+
+if(Test-Path "$path\.venv") {
+        Write-Output ".venv exists, not creating a new virtual environment."
+} else {
+        Write-Output "Creating virtual environment..."
+        python -m venv "$path\.venv"
+}
+
+if(Test-Path "$path\.git") {
+        Write-Output ".git exists, not creating a new repository."
+} else {
+        Write-Output "Initializing git..."
+        git init "$path"
+}
+
+Write-Output "Done.`n"
+
+Write-Host -NoNewLine "Press any key to open new project in VS Code (ESC to exit)..."
+$key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+if(!(27 -eq $key.VirtualKeyCode)) {
+        code $path
+}
+
+Write-Host "`n"
---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+* Python Bootstrap for VS Code (Windows)
+*
+* Use this if using MSYS2/bash-type terminals as VS Code default
+*
+* To get started use PowerShell to run:
+*   .\bootstrap <new project name>
+*
+* -- Yuri - Oct 2021
+"""
+
+if __name__==('__main__'):
+    print("It ain't much, but it's a start!")
